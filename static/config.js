bc9a81a1013abd772e49c670a953ac91b7f4e9d9
wangyunhui <1825548604@qq.com>
2018-11-16 10:02:25 +0800

    修改了在IE11上不能全屏的问题

--- static/config.js
+++ static/config.js
@@ -1,9 +1,11 @@
+var host=window.location.hostname;
+var base='http://'+host+':9988/yz';
 window.g = {
 	netName : '扬州市城市南部快速通道智能交通系统',    /*项目名字*/
 //	base : 'http://dxftech.asuscomm.com:8081/yz',    /*API接口前缀 - for 外网*/
 //	base : 'http://192.168.1.122:8081/yz',    /*API接口前缀*/
-//	base : 'http://192.168.1.250:8081',
-base : 'http://192.168.11.81:9988/yz',    //正式
+	// base : 'http://192.168.1.130:9988',   //本地
+	base : base,    //正式or测试
    // base : 'http://dxftech.asuscomm.com:9988/yz',  //测试
 }
 	
